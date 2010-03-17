4fff64c8c97189e9866d6965cf64d560a934d414
tyranus <Administrador@.(none)>
2010-03-17 17:07:08 -0300

    [Patch 14] Glyph of Siphon Life
    Signed-off-by: tyranus

---
+++ patchs-sql/patch14.sql
@@ -0,0 +1,3 @@
+-- Siphon Life
+DELETE FROM `spell_proc_event` WHERE `entry`='63108';
+INSERT INTO `spell_proc_event` VALUES ('63108', 0x00, 5, 0x00000002, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0.000000, 0.000000, 0);
